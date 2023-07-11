0f5e0e4e727ce9e8ef1c1feab1d50223331e7429
1san4ik <1san4ik@gmail.com>
2023-07-11 19:06:43 +0300

    Homework 10

---
+++ index.js
@@ -0,0 +1,3 @@
+const numberOfHours = Number(prompt('Введіть кількість годин: '))
+const numberOfSeconds = numberOfHours * 60**2
+alert(`Ви ввели ${numberOfHours} год., це дорівнює ${numberOfSeconds} секунд`)
